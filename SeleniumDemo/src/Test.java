34eeaf14ab94e96ba9da586e5c03cea92ac57e16
Bharath <bharathsworld@gmail.com>
2020-03-25 19:28:53 +0530

    corrected code

--- SeleniumDemo/src/Test.java
+++ SeleniumDemo/src/Test.java
@@ -1,4 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
-
+	public static void main(String[] args) {
+		
+		String exePath = "C:\\Bharath\\Selenium\\Driver\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver _driver = new ChromeDriver(); 
+		_driver.get("http://seleniumhq.org/");
+		//_driver.quit();
+	}
 }
